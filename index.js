4e8cb9b38fdedc5f8812fa3b7c2ad3b7f6d44cce
Jay Bhatt <jkb401.jb@gmail.com>
2022-08-30 10:06:34 -0400

    Simple Node Server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<html><strong>Hello World</strong></body></html>');
+        res.end();
+    }
+})
+
+server.listen(3000);
+
+console.log("Server is running on port 3000");
